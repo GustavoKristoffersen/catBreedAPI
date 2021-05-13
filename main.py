7fbc3e287b7672887679f1e2759b2ba62f0ef393
Gustavo William <gustavowilliam0980@gmail.com>
2021-05-13 10:18:06 -0300

    Created GET method for breeds list

--- main.py
+++ main.py
@@ -47,3 +47,31 @@ async def create(request: List[schemas.CatBreed], db: Session = Depends(get_db))
         response_breeds.append(new_breed)
 
     return response_breeds
+
+
+@app.get(
+    "/breeds", response_model=List[schemas.CatBreed], status_code=200, tags=["Breeds"]
+)
+async def get_list(
+    name: str = None,
+    location_of_origin: str = None,
+    coat_length: float = None,
+    body_type: str = None,
+    pattern: str = None,
+    db: Session = Depends(get_db),
+):
+    response = db.query(models.Breed)
+
+    if name:
+        response = response.filter_by(name=name)
+    if location_of_origin:
+        response = response.filter_by(location_of_origin=location_of_origin)
+    if coat_length:
+        response = response.filter_by(coat_length=coat_length)
+    if body_type:
+        response = response.filter_by(body_type=body_type)
+    if pattern:
+        response = response.filter_by(pattern=pattern)
+    
+    return response.all()
+    
